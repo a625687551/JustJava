e6e8b9ee53cf9d1addceb8c6ec34aeecb900a399
wangjianfeng <wangjianfeng@xueqiu.com>
2020-02-09 19:33:48 +0800

    add test

---
+++ src/main/java/aa.java
@@ -0,0 +1,12 @@
+/**
+ * @author ：wangjianfeng
+ * @date ：Created in 2020-02-09 19:32
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public class aa {
+    public static void main(String[] args) {
+        System.out.println("sfsfs");
+    }
+}
